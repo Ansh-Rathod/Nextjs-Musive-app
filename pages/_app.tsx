11546c03e47e01631f5c5c1adc5de21fee9901a6
Ansh-Rathod <anshrathod29@gmail.com>
2022-11-05 10:32:54 +0530

    updated bottom bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,16 +30,20 @@ function AudioPlayerComponent() {
       ) : (
         <div></div>
       )}
-      <div
-        className="bg-[#121212] hidden mobile:block tablet:block 
+      {router.pathname !== "/login" &&
+        router.pathname !== "/register" &&
+        router.pathname !== "/_error" && (
+          <div
+            className="bg-[#121212] hidden mobile:block tablet:block 
       fixed bottom-0 left-0 right-0 w-full pt-2 pb-1 z-20"
-      >
-        <div className="flex flex-row justify-center ">
-          <SidebarItem name="home" label="Home" />
-          <SidebarItem name="search" label="Search" />
-          <SidebarItem name="library" label="Library" />
-        </div>
-      </div>
+          >
+            <div className="flex flex-row justify-center ">
+              <SidebarItem name="home" label="Home" />
+              <SidebarItem name="search" label="Search" />
+              <SidebarItem name="library" label="Library" />
+            </div>
+          </div>
+        )}
     </div>
   );
 }
